a036c97a103a1fc653b04bbb30dfbeac1e6aebc0
Strife-Cyber <dunamisjunior15@gmail.com>
2025-07-15 16:16:36 +0100

    fichier ini complete

--- preprocessor/__init__.py
+++ preprocessor/__init__.py
@@ -1,7 +1,15 @@
 from cleaner_text.loader import load_text_data
 from cleaner_text.cleaner import clean_text_data
-from cleaner_text.augment import augment_dataframe
+from augment_text.augment import augment_dataframe
+
+from augment_image.image_augmentation import augment_image, augment_image_directory
+from cleaner_image.clean_image_data import load_and_clean_image, clean_image_directory
 
 VERSION = "1.0.0"
 
-__all__ = ["load_text_data", "clean_text_data", "augment_dataframe", "VERSION"]
+__all__ = [
+    "augment_image", "augment_image_directory",
+    "load_and_clean_image", "clean_image_directory",
+    "load_text_data", "clean_text_data", "augment_dataframe",
+    "VERSION"
+]
